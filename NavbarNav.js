9ff9f32be6f328c2589baa4ec1770e6225c77097
Davicho HF <39805786+David1044@users.noreply.github.com>
2021-08-07 10:28:45 -0500

    Add files via upload

---
+++ NavbarNav.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const NavbarNav = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Diagnostikare</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ms-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Usuario</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="http://localhost:3000/shop/?q=lo">API</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavbarNav;
---
+++ form.js
@@ -0,0 +1,50 @@
+import React from 'react';
+ import { useFormik } from 'formik';
+ 
+ const form = () => {
+
+   const formik = useFormik({
+     initialValues: {
+       firstName: '',
+       lastName: '',
+       email: '',
+     },
+     onSubmit: values => {
+       alert(JSON.stringify(values, null, 2));
+     },
+   });
+   return (
+     <form onSubmit={formik.handleSubmit}>
+       <label htmlFor="firstName">Nombre</label>
+       <input
+         id="firstName"
+         name="firstName"
+         type="text"
+         onChange={formik.handleChange}
+         value={formik.values.firstName}
+       />
+ 
+       <label htmlFor="lastName">Apellido</label>
+       <input
+         id="lastName"
+         name="lastName"
+         type="text"
+         onChange={formik.handleChange}
+         value={formik.values.lastName}
+       />
+ 
+       <label htmlFor="email">Correo</label>
+       <input
+         id="email"
+         name="email"
+         type="email"
+         onChange={formik.handleChange}
+         value={formik.values.email}
+       />
+ 
+       <button type="submit">Enviar</button>
+     </form>
+   );
+ };
+
+ export default form;
